d03d23f0dcea60a346bdad7848ced23234961c83
Marcin Olszowski <olszyna38@gmail.com>
2018-11-06 21:40:43 +0100

    Add files

---
+++ client/MessageList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './MessageList.css';
+
+const Message = props => (
+  	<div className={styles.Message}>
+    	<strong>{props.from} :</strong>
+    	<span>{props.text}</span>
+  	</div>
+);
+
+const MessageList = props => (
+  	<div className={styles.MessageList}>
+    	{
+      		props.messages.map((message, index) => {
+        		return (
+          			<Message
+            			key={index}
+            			from={message.from}
+            			text={message.text}
+          			/>
+        		);
+      		})
+    	}
+  	</div>
+);
+
+export default MessageList;
